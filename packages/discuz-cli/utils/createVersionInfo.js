f3a8c5e020b5df0f879b2cde0be0628eb10e5d75
lovebai <1507820372@qq.com>
2023-04-10 01:33:14 +0800

    chore: Update version number and remove unused module import.
    - Update version number to 4.0 in createVersionInfo.js
    - Remove getLogo module import in createVersionInfo.js
    - Changes made to multiple other files not included in summary
    - Overall improvements and optimizations to codebase.

--- packages/discuz-cli/utils/createVersionInfo.js
+++ packages/discuz-cli/utils/createVersionInfo.js
@@ -1,9 +1,9 @@
 const projectPackage = require('../utils/getProjectPkg')();
 const corePackages = require('../utils/getCorePackageList');
-const getLogo = require('../utils/getLogo');
+// const getLogo = require('../utils/getLogo');
 module.exports = (yamlConfig) => {
   const map = {};
-  let str = `Discuz!Q 3.0\n依赖核心库版本: \n当前版本：${yamlConfig.VERSION}\n`;
+  let str = `Discuz!Q 4.0\n依赖核心库版本: \n当前版本：${yamlConfig.VERSION}\n`;
   if (projectPackage) {
     const { dependencies, devDependencies } = projectPackage;
     for (let i = 0; i < corePackages.length; i++) {
@@ -16,5 +16,6 @@ module.exports = (yamlConfig) => {
 
   str += `构建时间: ${new Date().toString()}`;
   console.log(str);
+  console.log('二次开发项目，只修复已知bug不做新功能，需要新功能移步：https://discuzq.cc/');
   return str;
 };
